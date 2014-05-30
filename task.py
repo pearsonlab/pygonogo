cead9fd1b2be5afdd00cc95b67e1c06fafa66652
John Pearson <jmxpearson@gmail.com>
2014-05-30 11:11:30 -0400

    task object now takes task name and subject as inputs

--- task.py
+++ task.py
@@ -9,13 +9,17 @@ TO DO:
 import initializers
 
 class Task:
-    def __init__(self):
+    def __init__(self, taskname, subject):
+        self.taskname = taskname
+        self.subject = subject
         self.setup()
 
     def setup(self):
         self.pars = initializers.setup_pars("parameters.json")
         self.win = initializers.setup_window()
         self.geom = initializers.setup_geometry(self.win, self.pars)
+        self.outfile = initializers.setup_data_file(self.taskname, 
+            self.subject)
 
 
     def teardown(self):
